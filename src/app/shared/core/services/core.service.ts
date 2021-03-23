9222f77f0c1bcfe095fb9e88cd61da13b99ae05b
Carlos Enrique Ocas Quiroz <carlos.ocas@aysconsultores.pe>
2021-03-23 12:09:41 -0500

    utils core service

--- src/app/shared/core/services/core.service.ts
+++ src/app/shared/core/services/core.service.ts
@@ -2,8 +2,12 @@ import { Injectable } from '@angular/core';
 
 import { SweetAlertService } from './sweet-alert.service';
 
+import * as utils from "../../utils/types-convertion.utils";
+
 @Injectable()
 export class CoreService {
+
+    tools = utils;
 
     constructor(
         public sweetAlert: SweetAlertService
--- src/app/shared/utils/types-convertion.utils.ts
+++ src/app/shared/utils/types-convertion.utils.ts
@@ -1,3 +1,14 @@
+import { DatePipe } from "@angular/common";
+
+export function isNumber(value: any): boolean {
+  return !isNaN(this.toInteger(value));
+}
+
+export function isEmail(email: string) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email.toLowerCase());
+}
+
 /**
  * Convert string to Date
  *
@@ -7,7 +18,7 @@ export function getDateFromString(dateInStr: string = ''): Date {
   if (dateInStr && dateInStr.length > 0) {
     const dateParts = dateInStr.trim().split('/');
     const year = toInteger(dateParts[2]);
-    const month =  toInteger(dateParts[0]);
+    const month = toInteger(dateParts[0]);
     const day = toInteger(dateParts[1]);
     // tslint:disable-next-line:prefer-const
     let result = new Date();
@@ -20,11 +31,31 @@ export function getDateFromString(dateInStr: string = ''): Date {
   return new Date();
 }
 
-/**
- * Covert value to number
- *
- * @param value: any
- */
+export function dateToSend(_date: Date = new Date()): string {
+  let pipe = new DatePipe('en-US');
+  return pipe.transform(_date, 'yyyy-MM-dd');
+}
+
 export function toInteger(value: any): number {
   return parseInt(`${value}`, 10);
 }
+
+export function padNumber(value: number) {
+  if (this.isNumber(value)) {
+    return `0${value}`.slice(-2);
+  } else {
+    return '';
+  }
+}
+
+export function orderAlphabetically(list: any[], field: string): any[] {
+  if (list && list.length > 0) {
+    return list.sort(function (a, b) {
+      return a[field].toLowerCase().localeCompare(b[field].toLowerCase());
+    });
+  } else {
+    return [];
+  }
+}
+
+
